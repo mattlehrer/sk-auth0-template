62a8095bb1ccf43eb65adbe0458abada3a015022
Matt Lehrer <lehrerm@gmail.com>
2024-02-14 09:48:45 +0100

    setup open-props

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import postcssCustomMedia from 'postcss-custom-media';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [postcssCustomMedia()]
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
