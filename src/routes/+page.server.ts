7a60dd7746557a717b86c78074d14140e4ba9fee
Matt Lehrer <lehrerm@gmail.com>
2024-02-15 10:08:19 +0100

    add possible todo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,9 @@ import type { Actions, PageServerLoad } from './$types';
 import { lucia } from '$lib/server/auth';
 
 export const load: PageServerLoad = async ({ locals }) => {
+	// It's possible that the user will verify their email after the data is loaded from Auth0
+	// In that case, Auth0 does not push an update via webhook or otherwise.
+	// Consider using tanstack-query to poll for changes to the user's emailVerified status.
 	return { user: locals.user };
 };
 
